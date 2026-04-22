415f14d04acfeb02e61136d8aed616be24a1f9aa
Johannes Lindell <johannes152@hotmail.com>
2026-04-22 11:12:13 +0200

    fix: removed \n from println and added another println insted to avoid warning

--- repl.go
+++ repl.go
@@ -21,7 +21,8 @@ func commandExit(*config) error {
 
 func commandHelp(*config) error {
 	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:\n")
+	fmt.Println("Usage:")
+	fmt.Println("")
 
 	for _, cmd := range commands {
 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
